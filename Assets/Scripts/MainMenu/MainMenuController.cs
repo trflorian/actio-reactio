c5b5607ddc9b70aad4043ca97736dd46c78393d6
trflorian <florian.trautweiler@hispeed.ch>
2021-09-21 22:34:59 +0200

    added profile scene

--- Assets/Scripts/MainMenu/MainMenuController.cs
+++ Assets/Scripts/MainMenu/MainMenuController.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using PlayFab;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,8 +8,21 @@ namespace MainMenu
 {
     public class MainMenuController : MonoBehaviour
     {
-        private bool isSimulationRunning;
+        private const string FirstTimeKey = "FirstTime";
         
+        private bool isSimulationRunning;
+
+        private void Awake()
+        {
+            if (PlayerPrefs.GetInt(FirstTimeKey, 0) == 0)
+            {
+                // TODO remove
+                PlayerPrefs.SetInt(FirstTimeKey, 1);
+                PlayerPrefs.Save();
+                OpenProfile();
+            }
+        }
+
         public void LoadLevel(int level)
         {
             SceneManager.LoadScene(level == -1 ? SceneManager.sceneCountInBuildSettings - 1 : level + 1);
@@ -20,6 +36,11 @@ namespace MainMenu
         public void ShowScoreboard()
         {
             SceneManager.LoadScene("Scoreboard");
+        }
+
+        public void OpenProfile()
+        {
+            SceneManager.LoadScene("Profile");
         }
     }
 }
